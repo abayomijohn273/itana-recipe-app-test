b23d93e3d669d306df0263d41572984cc7df7a52
Olatunji Abayomi <abayomijohn273@gmail.com>
2023-05-27 14:03:34 +0100

    feat: added details modal

--- src/pages/home/components/recipeListingSection/index.js
+++ src/pages/home/components/recipeListingSection/index.js
@@ -1,14 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import RecipeCard from '../../../../components/blocks/recipeCard'
+import RecipeDetailsModal from '../../../../components/modal/recipeDetailsModal'
 
 const RecipeListingSection = () => {
+    const [showDetailsModal, setShowDetailsModal] = useState(false)
+    const [selectedDetails, setSelectedDetails] = useState({})
+
+    const handleShowModal = (data) => {
+        setShowDetailsModal(true);
+        setSelectedDetails(data)
+    }
+
+    const handleCloseModal = () => {
+        setShowDetailsModal(false)
+        setSelectedDetails({})
+    }
+
     return (
-        <div className="mt-8 lg:mt-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            <RecipeCard />
-            <RecipeCard />
-            <RecipeCard />
-            <RecipeCard />
-        </div>
+        <>
+            <div className="mt-8 lg:mt-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                <RecipeCard />
+                <RecipeCard />
+                <RecipeCard />
+                <RecipeCard />
+            </div>
+
+
+            <RecipeDetailsModal
+                open={showDetailsModal}
+                title={"Recipe Details"}
+                handleClose={() => handleCloseModal()}
+            />
+        </>
+
 
     )
 }
