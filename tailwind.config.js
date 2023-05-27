482a72bdc2f44b3c3d3898dbb5db02f3cca86b9d
Olatunji Abayomi <abayomijohn273@gmail.com>
2023-05-27 10:45:39 +0100

    feat: configure tailwind styles and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,29 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
+    "./src/*.{js,jsx,ts,tsx}",
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Nunito Sans", ...defaultTheme.fontFamily.sans],
+        serif: [...defaultTheme.fontFamily.serif],
+        mono: [...defaultTheme.fontFamily.mono],
+      },
+      fontSize: {
+        "sm-15": "0.9375rem", //15px
+      },
+      colors: {
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+        textColor: "var(--color-textColor)",
+        grayColor: "var(--color-grayColor)",
+      },
+    },
   },
   plugins: [],
 }
